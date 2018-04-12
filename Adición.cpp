d389c21de44f2b9f56657cc58565ea27825bf0a4
alejandrogrc <alejandrogrc@gmail.com>
2018-04-12 00:57:39 -0300

    Add files via upload

---
+++ Adición.cpp
@@ -0,0 +1,19 @@
+/* 
+ * Suma de dos numeros ingresados por el usuario y muestro en pantalla
+ * 20180412
+ *
+ * Author: Alejandro Gabriel Rodriguez Castro
+ */
+
+#include <iostream>
+
+
+int main()
+{
+	int numero1,numero2;
+    std::cout << "Ingrese Primer Numero:\n";
+    std::cin >> numero1;
+    std::cout << "Ingrese Segundo Numero:\n";
+    std::cin >> numero2;
+    std::cout << "La suma es: " << numero1 + numero2 << "\n";
+}
