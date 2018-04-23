821609141003059ac1c560ff6fca41a398e9641c
alejandrogrc <alejandrogrc@gmail.com>
2018-04-22 21:58:32 -0300

    Se cambia cout por assert y se agregan ejemplos de distintas  operaciones. Se modifica readme para ajustar el pseudo codigo a lo que se modifico del programa

--- 02-EjemploTipos/EjemploTipos.cpp
+++ 02-EjemploTipos/EjemploTipos.cpp
@@ -6,6 +6,8 @@
  */
 
 #include <iostream>
+#include <string>
+#include <cassert>
 
 int main()
 {
@@ -13,29 +15,14 @@ int main()
 	bool booleana=true;
 	char caracter='A';
 	unsigned sinsigno=900;
-	double doble=123235243523;
-	char cadena[12] = "Soy String";
+	double doble=150;
+	std::string cadena = "Soy String";
 
-	if (booleana == true)
-	{
-		std::cout << "La variable tipo bool tiene valor \"true\" y representa 1 bit en memoria\n";
-	}
-	if (entera == -10)
-	{
-		std::cout << "La variable tipo int tiene valor " << entera << " y tiene tamaño de " << sizeof(entera) << " Bytes\n";
-	}
-	if (caracter == 'A')
-	{
-		std::cout << "La variable tipo char tiene valor " << caracter << " y tiene tamaño de 1 Byte en memoria\n";
-	}
-	if (sinsigno == 900)
-	{
-		std::cout << "La variable tipo unsigned tiene valor " << sinsigno << " y tiene tamaño " << sizeof(sinsigno) << " Bytes, aparte no tiene signo negativo\n";
-	}
-	if (doble == 123235243523)
-	{
-		std::cout << "La variable tipo double tiene valor " << doble << " y tiene tamaño " << sizeof(doble) << " Bytes, tiene el doble de tamaño que la variable int\n";
-	}
-		std::cout << "La variable tipo string tiene valor " << cadena << " y tiene tamaño " << sizeof(cadena) << " Bytes\n";
-		std::cout << "Tambien existe la variable vacia \"void\", puntero null \"null pointer\" y las estructuras  \"struct\" que reunen conjunto de variables; entre otras\n";
+
+	assert(entera/10 == -1);
+	assert(booleana != false);
+	assert(caracter == 'A');
+	assert(sinsigno * 100 == 90000);
+	assert(doble + 2.5 == 152.5 );
+	assert(cadena.length() == 10);
 }
--- 02-EjemploTipos/readme.md
+++ 02-EjemploTipos/readme.md
@@ -1,12 +1,12 @@
-###_OS: Ubuntu 16.04 LTS_
+#### _OS: Ubuntu 16.04 LTS_
 
-##Compiler: gcc (Ubuntu 5.4.0-6ubuntu1~16.04.5) 5.4.0 20160609
+#### Compiler: gcc (Ubuntu 5.4.0-6ubuntu1~16.04.5) 5.4.0 20160609
 
-##Text Editor: Sublime
+#### Text Editor: Sublime
 
-##Descripcion del programa: Muestro en pantalla ejemplos de variables
+#### Descripcion del programa: Muestro en pantalla ejemplos de variables
 
-**Etapa #1: Analisis del problema**
+##**Etapa #1: Analisis del problema**
 
 _Problema:_ Diseñar un programa C++ que ejemplifique la aplicación de los tipos de datos vistos en clases.
 
@@ -21,17 +21,15 @@ No tiene modelo IPO al no tener entradas.
 
 _Lexico:_ entera E Z, booleana E B, caracter E Σ, sinsigno E N, doble E R, cadena E Σ*
 
-
 _Representación del Algoritmo:_
 
-    Representación textual: 
-        1) Mostrar booleana
-        2) Mostrar entera
-        3) Mostrar caracter
-        4) Mostrar sinsigno
-        5) Mostrar doble
-        6) Mostrar cadena
-        7) Mostrar "Texto"
+_Representación textual:_
+        1. Verifico que entera divida por 10 sea igual a -1
+        2. Verifico que booleana sea distinta de false
+        3. Verifico que caracter sea igual a 'A'
+        4. Verifico que sinsigno multiplicada por 100 sea 90000
+        5. Verifico que doble sumado a 2.5 sea igual a 152.5
+        6. Verifico que la longitud de cadena sea igual a 10
 
 _Credito Extra:_ Existen variables vacia (void), tipo struct (que reunen defierentes tipos  de variables en una esctructura), del tipo  puntero nulo (nullptr), entre otras variables.
     
